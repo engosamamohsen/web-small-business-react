f56ea35b2a6f5801dfa5b2eb7ade008505de6b66
Fathy-Said-Gonaim <fsaaed@trenddc.com>
2025-09-06 01:22:08 +0300

    set getSubdomain

--- src/app/subdomain.ts
+++ src/app/subdomain.ts
@@ -4,7 +4,8 @@ async function getSubdomain() {
   const host = headersList.get("host") || "";
   const subdomainValue = host.split(".")[0];
 
-  return process.env.NEXT_PUBLIC_BASE_URL ?? subdomainValue;
+  return subdomainValue;
+  // return process.env.NEXT_PUBLIC_BASE_URL ?? subdomainValue;
 }
 
 export default getSubdomain;
