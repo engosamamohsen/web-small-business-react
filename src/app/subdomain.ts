7dfa1b97f1b0b385db9d4e3bdb39acf86bd2330e
Fathy-Said-Gonaim <fsaaed@trenddc.com>
2025-09-21 23:43:56 +0300

    fix: use dynamic host URL instead of hardcoded domain

--- src/app/subdomain.ts
+++ src/app/subdomain.ts
@@ -4,8 +4,8 @@ async function getSubdomain() {
   const headersList = await headers();
   const host = headersList.get("host") || "";
   console.log("host", host);
-  // return `https://${host}`;
-  return "https://emendw.cashierthru.com";
+  return `https://${host}`;
+  // return "https://emendw.cashierthru.com";
 }
 
 export default getSubdomain;
