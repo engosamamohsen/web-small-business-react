91e9745f7581b0b495705e297504f23de575bc63
Fathy-Said-Gonaim <fsaaed@trenddc.com>
2025-11-15 03:13:36 +0200

    chore: bump version to 0.0.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,7 +112,7 @@ export default async function RootLayout({
 
         <Footer settingsData={settingResponse?.data} />
         <div className="flex w-full items-center justify-center bg-gray-500 py-4 text-center text-white">
-          v.0.0.1
+          v.0.0.2
         </div>
         <ToastContainer position="bottom-right" rtl />
       </body>
