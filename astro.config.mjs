21f4ca6d4ec57eb89b7b606fc44054262bf49dcc
Ahmed Elmasry <51840899+ahmedElmasry96@users.noreply.github.com>
2026-06-03 22:51:30 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cashierthru.com',  // base domain, not a specific tenant
   output: 'static',
+  adapter: node({ mode: 'standalone' }),
 
   integrations: [
     react(),
